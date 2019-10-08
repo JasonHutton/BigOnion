c4a1ff6c19bdb0ef5a59b66c35dd30e2fae8ac3d
ChengYe1995 <43919941+ChengYe1995@users.noreply.github.com>
2019-10-07 22:39:32 -0700

    sdl

---
+++ src/engine/UI.cpp
@@ -0,0 +1,26 @@
+#include "UI.h"
+
+UI::UI() {
+
+}
+
+void UI::init() {
+
+
+
+}
+/*
+int main(int argc, char* argv[]) {
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* window = SDL_CreateWindow("sdl",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		800, 600,
+		SDL_WINDOW_SHOWN);
+
+	SDL_Delay(3000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+}
+*/
+
---
+++ src/engine/UI.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include "SDL.h"
+#include <iostream>
+using namespace std;
+class UI
+{
+public:
+	UI();
+
+	void init();
+
+};
+
