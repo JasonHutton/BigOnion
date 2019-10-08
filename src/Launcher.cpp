2ff268177e507ac7a22e8b9ab39ca56845befa25
ChengYe1995 <43919941+ChengYe1995@users.noreply.github.com>
2019-10-08 02:15:56 -0700

    2 windoes
    sdl window and engine window

--- src/Launcher.cpp
+++ src/Launcher.cpp
@@ -2,6 +2,12 @@
 #include "game/game_loader.h"
 #include "engine/BOEngine.h"
 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include "SDL.h"
+#include <iostream>
+
 #define STB_IMAGE_IMPLEMENTATION
 
 
@@ -38,9 +44,33 @@ int runMain()
 		return 1;
 	}
 }
-
+/*
 int main()
 {
 	return runMain();
+}
+
+*/
+int main(int argc, char* argv[]) {
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* window = SDL_CreateWindow("sdl",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		800, 600,
+		SDL_WINDOW_SHOWN);
+
+	SDL_Delay(3000);
+	SDL_Event event;
+	while (1) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				exit(0);
+			}
+
+			return runMain();// game view
+		}
+	}
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
 }
 
--- src/engine/UI.cpp
+++ src/engine/UI.cpp
@@ -18,9 +18,19 @@ int main(int argc, char* argv[]) {
 		SDL_WINDOW_SHOWN);
 
 	SDL_Delay(3000);
+	SDL_Event event;
+	while (1) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				exit(0);
+			}
+		}
+	}
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
 }
 */
+
+
 
