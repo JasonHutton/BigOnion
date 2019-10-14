0bf36b7ce3e351e66a37445a5cd5f781d4e6567e
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2019-10-14 13:41:05 -0700

    Bad doubled-up temp implementation.

--- src/game/GameWorldHelper.cpp
+++ src/game/GameWorldHelper.cpp
@@ -7,19 +7,23 @@ void GameWorldHelper::initTestScene(BOEngine* engine, Shader* shader)
 	std::cout << "Game init" << std::endl;
 
 	// create suit man
+	string osSuitMan;
+	FileSystem::BuildOSPath(FileSystem::FindFile("game/assets/nanosuit/nanosuit.obj"), "game/assets/nanosuit/nanosuit.obj", osSuitMan);
 	GameObject* suitMan = new GameObject("SuitMan");
 	suitMan->transform.position = Vector3f(0, 20, 0);
 	suitMan->transform.rotation = Vector3f(45, 45, 45);
 	suitMan->transform.scale = Vector3f(0.2f, 0.2f, 0.2f);
-	suitMan->addComponent(new RenderComponent(engine, "src/game/assets/nanosuit/nanosuit.obj", shader)); // connect object - model
+	suitMan->addComponent(new RenderComponent(engine, osSuitMan, shader)); // connect object - model
 	suitMan->addComponent(RigidBodyComponent::createWithCylinder(0.75, 1.5, 0.25, 1.0)); // connect object - rigibody
 	engine->gameWorld.addGameObject(suitMan); // maybe auto register?
 
 	// create ground
+	string osGround;
+	FileSystem::BuildOSPath(FileSystem::FindFile("game/assets/ground/plane.obj"), "game/assets/ground/plane.obj", osGround);
 	GameObject* ground = new GameObject("Ground");
 	ground->transform.position = Vector3f(0, -3.25, 0);
 	ground->transform.scale = Vector3f(100.0f, 1.0f, 100.0f);
-	ground->addComponent(new RenderComponent(engine, "src/game/assets/ground/plane.obj", shader));
+	ground->addComponent(new RenderComponent(engine, osGround, shader));
 	ground->addComponent(RigidBodyComponent::createWithPlane());
 	engine->gameWorld.addGameObject(ground);
 
