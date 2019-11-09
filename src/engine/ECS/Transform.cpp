33247387e210955a4981089af7ce7f930bf3455f
wesley <107821628@qq.com>
2019-11-09 03:38:40 -0800

    renderer child model which relate to parent object

--- src/engine/ECS/Transform.cpp
+++ src/engine/ECS/Transform.cpp
@@ -8,23 +8,3 @@ Transform::Transform()
 	, scale()
 {
 }
-
-/*
-	Converts this Transform to a glm::mat4 transformation matrix For use in graphics.
-*/
-glm::mat4 Transform::toModelMatrix()
-{
-	glm::mat4 matrix = glm::mat4(1.0);
-
-	// translation
-	matrix = glm::translate(matrix, position.getGlmVec3());
-
-	// rotation
-	glm::mat4 rotationalMatrix = glm::eulerAngleZYX(rotation.z, rotation.y, rotation.x);
-	matrix = matrix * rotationalMatrix;
-
-	// scale
-	matrix = glm::scale(matrix, scale.getGlmVec3());
-
-	return matrix;
-}
