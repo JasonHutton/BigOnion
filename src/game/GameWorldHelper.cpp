456da3614d9f60e7f44b12dd4feff8a8af06f23b
Paul1005 <mccarliep@gmail.com>
2019-11-10 00:29:50 -0800

    deleted testing boxes

--- src/game/GameWorldHelper.cpp
+++ src/game/GameWorldHelper.cpp
@@ -141,30 +141,7 @@ void GameWorldHelper::initTestScene(BOEngine* engine)
 	ground->addComponent(RigidBodyComponent::createWithPlane());
 	engine->gameWorld->addGameObject(ground);
 
-	// create box
-	GameObject* box = new  GameObject("Box");
-	box->transform.position = Vector3f(5.0, 10.0, 0);
-	box->transform.scale = 2.0; // has to be double because dimensions of 1.0 entered above refer to distance from origin to edge
-	box->addComponent(new RenderComponent(engine, "game/assets/box/cube.obj", shader));
-	box->addComponent(RigidBodyComponent::createWithCube(1.0, 1.0, 1.0, 1.0, 0.0));
-	engine->gameWorld->addGameObject(box);
-
-	GameObject* box2 = new  GameObject("Box2");
-	box2->transform.position = Vector3f(5.0, 15.0, 0);
-	box2->transform.scale = 2.0; // has to be double because dimensions of 1.0 entered above refer to distance from origin to edge
-	box2->addComponent(new RenderComponent(engine, "game/assets/box/cube.obj", shader));
-	box2->addComponent(RigidBodyComponent::createWithCube(1.0, 1.0, 1.0, 1.0, 0.0));
-	engine->gameWorld->addGameObject(box2);
-
-
-	GameObject* box3 = new  GameObject("Box3");
-	box3->transform.position = Vector3f(5.0, 20.0, 0);
-	box3->transform.scale = 2.0; // has to be double because dimensions of 1.0 entered above refer to distance from origin to edge
-	box3->addComponent(new RenderComponent(engine, "game/assets/box/cube.obj", shader));
-	box3->addComponent(RigidBodyComponent::createWithCube(1.0, 1.0, 1.0, 1.0, 0.0));
-	engine->gameWorld->addGameObject(box3);
-
-	GameObject* box4 = new  GameObject("Box4");
+	/*GameObject* box4 = new  GameObject("Box4");
 	box4->transform.position = Vector3f(5.0, 25.0, 0);
 	box4->transform.scale = 2.0; // has to be double because dimensions of 1.0 entered above refer to distance from origin to edge
 	box4->addComponent(new RenderComponent(engine, "game/assets/box/cube.obj", shader));
@@ -172,5 +149,5 @@ void GameWorldHelper::initTestScene(BOEngine* engine)
 	box4->addComponent(new TypeTestComponent("This is a test message!"));
 	engine->gameWorld->addGameObject(box4);
 
-	box4->getComponent<TypeTestComponent>()->talk();
+	box4->getComponent<TypeTestComponent>()->talk();*/
 }
